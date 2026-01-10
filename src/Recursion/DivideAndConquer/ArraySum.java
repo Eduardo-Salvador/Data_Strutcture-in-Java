327b95adb94bc15e7c1ddbf8b8864fefda11e648
Eduardo-Salvador <eduardo.salvador.gs@gmail.com>
2026-01-10 18:12:12 -0300

    ✨ [feat] Recursion: Divide And Conquer

--- src/Recursion/DivideAndConquer/ArraySum.java
+++ src/Recursion/DivideAndConquer/ArraySum.java
@@ -2,20 +2,20 @@ package Recursion.DivideAndConquer;
 
 
 public class ArraySum {
-    public static Integer sum(Integer[] array) {
-        int start = array[0];
-        int sumNums = 0;
-        if (!array[0].equals(array[array.length - 1])){
-            //array[0] = array[];
-            sumNums = start + sum(array);
+    private static Integer sumHelper(Integer[] array, int index){
+        if(index >= array.length){
+            return 0;
         }
-        return start + sumNums;
+        return array[index] + sumHelper(array, index + 1);
+    }
+
+    public static Integer sum(Integer[] array) {
+        return sumHelper(array, 0);
     }
 
     public static void main(String[] args) {
         Integer[] arrayBase = new Integer[]{0,1,2,3,4,5,6,7,8,9};
         Integer result = ArraySum.sum(arrayBase);
-        System.out.println(result);
-
+        System.out.println("Array Sum: " + result);
     }
 }
---
+++ src/Recursion/DivideAndConquer/MaxValueInListArray.java
@@ -0,0 +1,36 @@
+package Recursion.DivideAndConquer;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MaxValueInListArray {
+    public static Integer max(List<Integer> list, int index, Integer maxValue){
+        if (index >= list.size()){
+            return maxValue;
+        }
+        if (list.get(index) > maxValue){
+            maxValue = list.get(index);
+        }
+        return max(list, (index + 1), maxValue);
+    }
+
+    public static Integer max(Integer[] array, int index, Integer maxValue){
+        if (index >= array.length){
+            return maxValue;
+        }
+        if (array[index] > maxValue){
+            maxValue = array[index];
+        }
+        return max(array, (index + 1), maxValue);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(List.of(0,1,2,3,4,5,6,7,8,9,11,23,42,12));
+        Integer resultList = max(list, 0, 0);
+
+        Integer[] array = new Integer[]{0,1,44,23,192,321,4,2,1,1};
+        Integer resultArray = max(array, 0, 0);
+
+        System.out.println("Max in List: " + resultList);
+        System.out.println("Max in Array: " + resultArray);
+    }
+}
